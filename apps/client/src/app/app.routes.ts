5bb41473b1eb647ce632e6245dc667123d2a6270
denis dom <ddenis1994@gmail.com>
2023-07-12 20:08:19 +0300

    remove nx

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./task/feature/task-editing/task-editing.module').then(
+        (m) => m.TaskEditingModule
+      ),
+  },
+];
