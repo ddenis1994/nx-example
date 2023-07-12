d068a8e8456e87baaae18f7ccc749444cb603ab1
denis dom <ddenis1994@gmail.com>
2023-07-12 20:53:17 +0300

    init all pages

---
+++ apps/client/src/app/task/feature/scheduling/scheduling.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SchedulingRoutingModule } from './scheduling-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, SchedulingRoutingModule],
+})
+export class SchedulingModule {}
--- apps/client/src/app/task/feature/task-editing/task-editing-routing.module.ts
+++ apps/client/src/app/task/feature/task-editing/task-editing-routing.module.ts
@@ -1,4 +1,4 @@
-import { Route, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { TaskEditingComponent } from './task-editing.component';
 import { NgModule } from '@angular/core';
 
@@ -6,6 +6,37 @@ const routes: Routes = [
   {
     path: '',
     component: TaskEditingComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('../process/process.module').then((m) => m.ProcessModule),
+      },
+      {
+        path: 'process',
+        loadChildren: () =>
+          import('../process/process.module').then((m) => m.ProcessModule),
+      },
+      {
+        path: 'scheduling',
+        loadChildren: () =>
+          import('../scheduling/scheduling.module').then(
+            (m) => m.SchedulingModule
+          ),
+      },
+      {
+        path: 'distribution',
+        loadChildren: () =>
+          import('../distribution/distribution.module').then(
+            (m) => m.DistributionModule
+          ),
+      },
+      {
+        path: 'output',
+        loadChildren: () =>
+          import('../output/output.module').then((m) => m.OutputModule),
+      },
+    ],
   },
 ];
 
